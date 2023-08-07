840119cdc4cec7b4f3ee86b4d52ea7a2d8b55567
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-07 18:46:21 +0100

    created the components for the contactManager app

---
+++ src/app/contactmanager/contactmanager-app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactmanager-app',
+  template: `
+    <p>
+      contactmanager-app works!
+    </p>
+  `,
+  styleUrls: ['./contactmanager-app.component.scss']
+})
+export class ContactmanagerAppComponent {
+
+}
