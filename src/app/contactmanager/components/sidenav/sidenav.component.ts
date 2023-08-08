5765e8c174d4e411751f99af5267ece119d7168b
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-08 17:05:01 +0100

    added a sidenav and a toggle button

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout'
+
+const SMALL_WIDTH_BREAKPOINT: number = 720;
 
 @Component({
   selector: 'app-sidenav',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class SidenavComponent {
 
+  public isScreenSmall!: boolean;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+    .observe([ `max-width: ${SMALL_WIDTH_BREAKPOINT}px` ])
+    .subscribe((state: BreakpointState) => {
+      this.isScreenSmall = state.matches;   
+    });
+  }
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatCard, MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   exports: [
     MatButtonModule,
     MatIconModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
